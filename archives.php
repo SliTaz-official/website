cadb3b5b732146bb8865becb7ff8573efdedea56
Pascal Bellard <pascal.bellard@slitaz.org>
2010-09-28 17:53:15 +0200

    mailing-list.html: add language support in archive index

--- archives.php
+++ archives.php
@@ -33,12 +33,70 @@
 	</style>
 </head>
 <body>
-<h4>Archives 2007-<?php
-echo date('Y').'</h4>';
+<?php
+
+function translate($word)
+{
+	if (isset($_GET['lang'])) switch ($_GET['lang']) {
+	case "de" :
+		switch ($word) {
+		case "Archives"  : return "Archiv";
+		case "Archive"   : return "Archiv";
+		case "View by"   : return "Ansicht von";
+		case "Number"    : return "Zahl";
+		case "Total size": return "Gesamtgr\&ouml;\&szlig;e";
+		case " or "      : return " oder ";
+		case "Date"      : return "datum";
+		}
+		break;
+	case "es" :
+		switch ($word) {
+		case "Archives"  : return "Archivos";
+		case "Archive"   : return "Archivo";
+		case "View by"   : return "Vista";
+		case "Number"    : return "n\&uacute;mero";
+		case "Total size": return "Tama\&ntilde;o total";
+		case "Thread"    : return "hilo";
+		case " or "      : return " o ";
+		case "Date"      : return "Fecha";
+		}
+		break;
+	case "fr" :
+		switch ($word) {
+		case "View by"   : return "Affich\&eacute; par";
+		case "Number"    : return "Nombre";
+		case "Total size": return "Volume";
+		case "Thread"    : return "Sujet";
+		case " or "      : return " ou ";
+		}
+		break;
+	case "pt" :
+		switch ($word) {
+		case "Archives"  : return "Arquivos";
+		case "Archive"   : return "Arquivo";
+		case "View by"   : return "Exibir por";
+		case "Number"    : return "n\&uacute;mero";
+		case "Total size": return "O tamanho total";
+		case "Thread"    : return "T\&oacute;pico";
+		case " or "      : return " ou ";
+		case "Date"      : return "Data";
+		}
+		break;
+	}
+	return $word;
+}
+
+echo '<h4>'.translate("Archives").' 2007-'.date('Y')."</h4>\n";
+
 $url = "http://listengine.tuxfamily.org/lists.tuxfamily.org/slitaz/";
+
 $script  = "-e '/<table>/,/<\/table>/!d' ";
 $script .= "-e 's|html\"|html\" target=\"_blank\"|g' ";
 $script .= "-e 's|href=\"|href=\"$url|g' ";
+foreach (array("Archive","View by","Number","Total size",
+	       "Thread"," or ","Date") as $word) {
+	$script .= "-e 's|".$word."|".translate($word)."|' ";
+}
 system("wget -q -O - $url | sed $script");
 ?>
 </body>
