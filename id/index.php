d30676e5defb602229c30c0f58ea33f7a3222942
Christophe Lincoln <pankso@slitaz.org>
2011-03-26 22:22:15 +0100

    ex + id: Code clean-up and new layout (TODO: finish es)

---
+++ id/index.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<title>SliTaz GNU/Linux (id)</title>
+	<meta http-equiv="content-type" content="text/html; charset=ISO-8859-1" />
+	<meta name="description" content="SliTaz GNU/Linux LiveCD" />
+	<meta name="keywords" lang="en" content="System, free, gnu, linux, software opensource, livecd LINUX in RAM" />
+	<meta name="robots" content="index, follow, all" />
+	<meta name="modified" content="2011-03-26 22:00:00" />
+	<meta name="author" content="Budiarno"/>
+	<meta name="publisher" content="www.slitaz.org" />
+	<link rel="shortcut icon" href="../favicon.ico" />
+	<link rel="stylesheet" type="text/css" href="../slitaz.css" />
+	<link rel="alternate" type="application/rss+xml" title="SliTaz Website feed" href="../en/rss.xml" />
+	<link rel="alternate" type="application/atom+xml" title="SliTaz Labs feed" href="http://labs.slitaz.org/news?format=atom" />
+</head>
+<body>
+
+<!-- Header -->
+<div id="header">
+	<div id="logo"></div>
+	<div id="network">
+		<a href="http://www.slitaz.org/netmap.php">
+			<img src="../images/network.png" alt="network.png" /></a>
+		<a href="http://scn.slitaz.org/">Community</a>
+		<a href="http://doc.slitaz.org/">Doc</a>
+		<a href="http://forum.slitaz.org/">Forum</a>
+		<a href="http://labs.slitaz.org/issues">Bugs</a>
+		<a href="http://hg.slitaz.org/">Hg</a>
+	</div>
+	<h1><a href="http://www.slitaz.org/">SliTaz GNU/Linux</a></h1>
+</div>
+
+<!-- Block -->
+<div id="block">
+	<!-- Navigation -->
+	<div id="block_nav">
+		<h4>Navigation</h4>
+		<div class="right_box">
+			<ul>
+				<li><a href="../en/mailing-list.html">Mailing List</a></li>
+				<li><a href="../en/devel/">Development</a></li>
+				<li><a href="../en/packages/">Packages</a></li>
+				<li><a href="../en/artwork/">Artwork</a></li>
+				<li><a href="../en/search.html">Search</a></li>
+			</ul>
+		</div>
+		<div class="left_box">
+			<ul>
+				<li><a href="./">Headlines</a></li>
+				<li><a href="../en/about/">About</a></li>
+				<li><a href="../en/get/">Download</a></li>
+				<li><a href="../en/asso/">Association</a></li>
+				<li><a href="../en/doc/">Documentation</a></li>
+			</ul>
+		</div>
+	</div>
+	<!-- Information/image -->
+	<div id="block_info">
+		<h4>Halaman Utama</h4>
+		<p>
+			SliTaz didistribusikan dalam bentuk LiveCD dan berukuran
+			kurang lebih 30MB. Sistemnya cepat dan responsif, bersih
+			dan dukungan yang kuat. SliTaz itu sederhana dan intuitif,
+			menyediakan desktop yang elegan tetapi tetap ringan, tool
+			konfigurasi yang mudah dan terdokumentasi dengan detil.
+		</p>
+	</div>
+</div>
+
+<!-- Languages -->
+<div id="lang">
+	<a href="../i18n.php">
+		<img src="../images/locale.png" alt="locale.png" /></a>
+	<a href="../de/">Deutsch</a>
+	<a href="../en/">English</a>
+	<a href="../es/">Español</a>
+	<a href="../fr/">Français</a>
+	<a href="../id/">Indonesian</a>
+	<a href="../pt/">Português</a>
+	<a href="../cn/">Chinese</a>
+</div>
+
+<!-- Content -->
+<div id="content">
+
+<!--
+    SliTaz news starting.
+-->
+
+<h2>Headlines</h2>
+
+<div class="box">
+	<img src="../images/news.png" alt="news.png" />
+	<a href="http://labs.slitaz.org/news">Berita dari Labs</a>
+	(<a href="http://labs.slitaz.org/news?format=atom">Feed</a>) | 
+	<a href="http://doc.slitaz.org/en:newsletter:start">Newsletter Bulanan</a>
+</div>
+
+<div id="news">
+
+<ul>
+	<!-- <li><strong>Date - Title</strong>
+	<p></p>
+	</li> -->
+	
+	<li><strong>28 Mar 2010 - SliTaz GNU/Linux 3.0 pelepasan</strong>
+	<p>Setelah satu tahun masa pengembangan, SliTaz versi 3.0 telah dirilis.
+	Sekarang lebih simple, lebih cepat dan lebih kecil (ukuran filenya),
+	diperkuat dengan sekira 2300 file paket. Desktopnya menggunakan
+	LXPanel, openbox dan Xorg 7.4. Aplikasi bawaan berupa Midori, Rysnc,
+	Alsa, ePDFview dan gFTP. Toolchain baru termasuk GCC 4.4.1 dan kernel
+	2.6.30.6. System intinya berupa file image sebesar 30MB dan LiveCD
+	mulai dari 8MB. Anda bisa baca catatan rilis untuk informasi
+	selanjutnya dan download dari salah satu mirror resmi project ini.
+	</p></li>
+</ul>
+
+<!-- End of news -->
+</div>
+
+<!-- End of content -->
+</div>
+
+<!-- Footer -->
+<div id="footer">
+	Copyright &copy; <span class="year"></span>
+	<a href="http://www.slitaz.org/">SliTaz</a> - Network:
+	<a href="http://scn.slitaz.org/">Community</a>
+	<a href="http://doc.slitaz.org/">Doc</a>
+	<a href="http://forum.slitaz.org/">Forum</a>
+	<a href="http://pkgs.slitaz.org/">Packages</a>
+	<a href="http://labs.slitaz.org/issues">Bugs</a>
+	<a href="http://hg.slitaz.org/">Hg</a>
+	<p>
+		SliTaz @
+		<a href="http://twitter.com/slitaz">Twitter</a>
+		<a href="http://www.facebook.com/slitaz">Facebook</a>
+		<a href="http://distrowatch.com/slitaz">Distrowatch</a>
+		<a href="http://en.wikipedia.org/wiki/SliTaz">Wikipedia</a>
+		<a href="http://flattr.com/profile/slitaz">Flattr</a>
+	</p>
+</div>
+
+</body>
+</html>
+
