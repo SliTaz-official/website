be12782104631f65fdbf14164488732842cd1aa8
Christophe Lincoln <pankso@slitaz.org>
2011-03-30 17:27:20 +0200

    get-feed.php: clean up, improve speed (get 4 last entry and break), show up hour

--- libs/get-feed.php
+++ libs/get-feed.php
@@ -4,25 +4,19 @@
 function get_rss_feed($feed_url) {
 	$content = file_get_contents($feed_url);
 	$x = new SimpleXmlElement($content);
+	$entries = 4;
+	$count = 0;
 	// We look for last item, channel may not have a PubDate*
 	$up = ($x->channel->item->pubDate);
-	echo "<span>Updated: " . substr("$up", 5, 12) . "</span>\n";
+	echo "<span>Updated: " . substr("$up", 5, 17) . "</span>\n";
 	echo "</p>\n<div>\n<ul>\n";
 	foreach($x->channel->item as $entry) {
-		echo "	<li><a href='$entry->link' title='$entry->title'>" . $entry->title . "</a></li>\n";
-	}
-	echo "</ul>\n";
-}
-
-// Read Atom Feed
-function get_atom_feed($feed_url) {
-	$content = file_get_contents($feed_url);
-	$x = new SimpleXmlElement($content);
-	$up = ($x->updated);
-	echo "<span>Updated: " . substr("$up", 0, 10) . "</span>\n";
-	echo "</p>\n<div>\n<ul>\n";
-	foreach($x->entry as $entry) {
-		echo "	<li><a href='$entry->id' title='$entry->title'>" . $entry->title . "</a></li>\n";
+		$count = $count + 1;
+		echo "	<li><a href='$entry->link' title='$entry->title'>" . 
+			$entry->title . "</a></li>\n";
+		if ($count == $entries) {
+			break;
+		}
 	}
 	echo "</ul>\n";
 }
