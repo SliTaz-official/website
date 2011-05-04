8aed052f9be7cfef7b0fcf9f34b2a35f827a5dd7
Paul Issott <paul@slitaz.org>
2011-05-04 15:58:14 +0100

    es: add devel (thanks Eduardo)

---
+++ es/devel/index.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es" lang="es">
+<head>
+    <title>SliTaz - Desarrollo</title>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+    <meta name="description" content="SliTaz GNU/Linux project information overview" />
+    <meta name="keywords" lang="es" content="about slitaz, gnu, linux, mini distro, livecd" />
+    <meta name="robots" content="index, follow, all" />
+    <meta name="modified" content="<?php echo (date( "Y-m-d H:i:s", getlastmod())); ?>" />
+    <meta name="author" content="Eduardo Suarez-Santana" />
+    <link rel="shortcut icon" href="../../favicon.ico" />
+    <link rel="stylesheet" type="text/css" href="../../slitaz.css" />
+</head>
+<body>
+
+<!-- Header -->
+<div id="header">
+	<div id="logo"></div>
+	<div id="network">
+		<a href="http://www.slitaz.org/netmap.php">
+			<img src="../../images/network.png" alt="network.png" /></a>
+		<a href="http://scn.slitaz.org/">Community</a>
+		<a href="http://doc.slitaz.org/">Doc</a>
+		<a href="http://forum.slitaz.org/">Forum</a>
+		<a href="http://bugs.slitaz.org">Bugs</a>
+		<a href="http://hg.slitaz.org/">Hg</a>
+	</div>
+	<h1><a href="http://www.slitaz.org/">SliTaz GNU/Linux</a></h1>
+</div>
+
+<!-- Block -->
+<div id="block">
+	<!-- Navigation -->
+	<div id="block_nav">
+		<h4>Navigation</h4>
+		<div class="right_box">
+			<ul>
+				<li><a href="../mailing-list.html">Lista de correo</a></li>
+				<li><a href="../devel/">Desarrollo</a></li>
+				<li><a href="../packages/">Paquetes</a></li>
+				<li><a href="../artwork/">Artwork</a></li>
+				<li><a href="../search.html">Buscar</a></li>
+			</ul>
+		</div>
+		<div class="left_box">
+			<ul>
+				<li><a href="../">Titulares</a></li>
+				<li><a href="../about/">Acerca de</a></li>
+				<li><a href="../get/">Descargas</a></li>
+				<li><a href="../asso/">Asociación</a></li>
+				<li><a href="../doc/">Documentación</a></li>
+			</ul>
+		</div>
+	</div>
+	<!-- Information/image -->
+	<div id="block_info">
+		<h4>Desarrollo</h4>
+		<p>
+			SliTaz es una distribución comunitaria hecha por mucha gente alrededor
+			del mundo. Todo el mundo es bienvenido a ayudar y a involucrarse en el
+			proyecto.
+		</p>
+		<p>
+			<img src="../../images/users.png" alt="users.png" />
+			<a href="http://scn.slitaz.org/">Únete a nosotros en SCN</a> y
+			en la <a href="../mailing-list.html">la lista de correo</a>
+		</p>
+	</div>
+</div>
+
+<!-- Content -->
+<div id="content">
+
+<h2>Rincón del desarrollador</h2>
+
+<ul>
+	<li><a href="../../en/devel/forge.php">Forja</a> - Gestión colaborativa, KISS, líneas directrices, 
+	respositorios de Mercurial, página web y otros servicios.</li>
+	<li><a href="../../i18n.php">Proyecto de internacionalización</a> -
+	Traducciones, metas y administración.</li>
+	<li><a href="http://hg.slitaz.org/" >Repositorios de Mercurial</a></li>
+	<li><a href="http://bugs.slitaz.org/">Gestor de Incidencias de SliTaz</a></li>
+</ul>
+
+<a name="devel"></a>
+<h2>Involúcrate</h2>
+<p>
+	SliTaz es una distribución comunitaria y de código abierto. Todo el
+	mundo es bienvenido a unirse y contribuir. Para usuarios, manitas y
+	desarrolladores, siempre hay algo que hacer, como verificar
+	o escribir documentación, enviar errores o parches a la lista de correo,
+	conseguir acceso al wok y añadir algunos paquetes, o simplemente ayudar
+	a otros en la lista de correo o en el <a href="http://forum.slitaz.org/">foro</a>. 
+	SliTaz tiene repositorios de Mercurial sobre un sistema también SliTaz. 
+	Los desarrolladores pueden solicitar un nuevo repositorio si fuera necesario 
+ 	y cualquiera puede contribuir con acceso de escritura para corregir errores 
+	tipográficos, añadir scripts, etc.
+</p>
+<p>
+	SliTaz es una comunidad pequeña que escucha a sus usuarios. Hay varios
+	desarrolladores activos en el <a href="http://forum.slitaz.org/">foro</a>
+ 	y en la <a href="../mailing-list.html">lista de correo</a>.
+</p>
+<p>
+	Incluso para artistas hay una web dedicada. Simplemente crea una cuenta
+	y envía tus gráficos o imágenes. La web está gestionada por la
+	comunidad. Si quieres echar una mano administrando la web, por favor
+	contacta con un colaborador o escribe a la lista de correo. Únete al
+	<a href="http://scn.slitaz.org/groups/artwork/">Artwork group</a> de la 
+	Comunidad SliTaz.
+</p>
+
+<!-- End of content -->
+</div>
+
+<!-- Footer -->
+<div id="footer">
+	Copyright &copy; <span class="year"></span>
+	<a href="http://www.slitaz.org/">SliTaz</a> - Network:
+	<a href="http://scn.slitaz.org/">Community</a>
+	<a href="http://doc.slitaz.org/">Doc</a>
+	<a href="http://forum.slitaz.org/">Forum</a>
+	<a href="http://pkgs.slitaz.org/">Packages</a>
+	<a href="http://bugs.slitaz.org">Bugs</a>
+	<a href="http://hg.slitaz.org/">Hg</a>
+	<p>
+		SliTaz @
+		<a href="http://twitter.com/slitaz">Twitter</a>
+		<a href="http://www.facebook.com/slitaz">Facebook</a>
+		<a href="http://distrowatch.com/slitaz">Distrowatch</a>
+		<a href="http://en.wikipedia.org/wiki/SliTaz">Wikipedia</a>
+		<a href="http://flattr.com/profile/slitaz">Flattr</a>
+	</p>
+</div>
+
+</body>
+</html>
