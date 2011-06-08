b8d727a23d168402332099d83e63c8e8b4d1be52
Bo Vagner Hoejer <bohoejer@live.dk>
2011-06-08 08:36:13 +0200

    da: fixes and devel section

---
+++ da/devel/index.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="da" lang="da">
+<! Danish (da) translation by Bo Vagner Højer - bohoejer@slitaz.org
+   Member of Labitat Hackerspace, Copenhagen - www.labitat.dk
+   visit our hackerspace every tuesday evening for free - always hot coffee >
+<head>
+    <title>SliTaz - Udviklernes hjørne</title>
+    <meta http-equiv="content-type" content="text/html; charset=ISO-8859-1" />
+    <meta name="description" content="SliTaz GNU/Linux projekt overblik" />
+    <meta name="keywords" lang="da" content="om slitaz, gnu, linux, minidistribution, livecd" />
+    <meta name="robots" content="index, follow, all" />
+    <meta name="modified" content="<?php echo (date( "Y-m-d H:i:s", getlastmod())); ?>" />
+    <meta name="author" content="Linux User"/>
+    <link rel="shortcut icon" href="../../favicon.ico" />
+    <link rel="stylesheet" type="text/css" href="../../slitaz.css" />
+</head>
+<body>
+
+<!-- Header -->
+<div id="header">
+	<div id="logo"></div>
+	<div id="network">
+		<a href="http://www.slitaz.org/netmap.php">
+			<img src="../../images/network.png" alt="netværk:" /></a>
+		<a href="http://scn.slitaz.org/">Samfund</a>
+		<a href="http://doc.slitaz.org/">Vejledning</a>
+		<a href="http://forum.slitaz.org/">Forum</a>
+		<a href="http://bugs.slitaz.org">Fejl</a>
+		<a href="http://hg.slitaz.org/">Hg</a>
+	</div>
+	<h1><a href="http://www.slitaz.org/">SliTaz GNU/Linux</a></h1>
+</div>
+
+<!-- Block -->
+<div id="block">
+	<!-- Navigation -->
+	<div id="block_nav">
+		<h4>Navigation</h4>
+		<div class="right_box">
+			<ul>
+				<li><a href="../mailing-list.html">Postliste</a></li>
+				<li><a href="../devel/">Udvikling</a></li>
+				<li><a href="../packages/">Programpakker</a></li>
+				<li><a href="../artwork/">Grafik</a></li>
+				<li><a href="../search.html">Søgning</a></li>
+			</ul>
+		</div>
+		<div class="left_box">
+			<ul>
+				<li><a href="../">Nyheder</a></li>
+				<li><a href="../about/">Om SliTaz</a></li>
+				<li><a href="../get/">Hent SliTaz</a></li>
+				<li><a href="../asso/">Organisation</a></li>
+				<li><a href="../doc/">Vejledning</a></li>
+			</ul>
+		</div>
+	</div>
+	<!-- Information/image -->
+	<div id="block_info">
+		<h4>Udvikling</h4>
+		<p>
+		   SliTaz er en netværksbaseret distribution lavet af mange
+		   mennesker fra hele verdenen og alle er velkommen til at
+		   hjælpe og blive indvolveret i projektet.
+		</p>
+		<p>
+			<img src="../../images/users.png" alt="users.png" />
+			<a href="http://scn.slitaz.org/">Følg os på SCN</a> og
+			<a href="../mailing-list.html">postlisten</a>
+		</p>
+	</div>
+</div>
+
+<!-- Content -->
+<div id="content">
+
+<h2>Udviklernes hjørne</h2>
+
+<ul>
+	<li><a href="forge.php">Forge</a> - Samarbejdende styring, KISS,
+	retningslinier, Mercurial depoter, hjemmeside og andre tjenester.</li>
+	<li><a href="../../i18n.php">Internationaliseringsprojekt</a> -
+	Oversættelse, mål og styring.</li>
+	<li><a href="http://hg.slitaz.org/" >Mercurial depoter</a></li>
+	<li><a href="http://bts.slitaz.org/">SliTaz Fejlsporing</a></li>
+</ul>
+
+<a name="devel"></a>
+<h2>Bliv aktiv</h2>
+<p>
+   SliTaz er en brugerdrevet åben kildekode distribution. Alle, fra brugere, til hackere
+   og udviklere, er velkommen til at deltage og bidrage, der er altid noget at gøre.
+    For eksempel at læse korrektur eller skrive dokumentation, sende fejl eller rettelser
+    til postlisten, få adgang til wokken og skub nogle nye programpakker op
+    eller simpelt hen at hjælpe andre på postlisten 
+	eller <a href="http://forum.slitaz.org/">forumet</a>. 
+	SliTaz har Mercurial
+	depoter lagt på et SliTaz system, udviklere kan efter behov bede om et nyt depot
+	og bidragydere har skriveadgang for at rette slå- og stavefejl , scripts, etc.
+</p>
+<p>
+	SliTaz er et lille netværkssamfund og det lytter til dets brugere.
+	Der er mange udviklere, 
+	som er aktive på <a href="http://forum.slitaz.org/">forumet</a>
+	og <a href="../mailing-list.html">postlisten</a>.
+</p>
+<p>
+   For grafikere og kunstnere er der en dedikeret hjemmeside, hvor du 
+   kan poste grafik og billeder, efter at have oprettet en konto.
+	Hjemmesiden er drevet af netværkssamfundet. Hvis du vil give en hånd
+	med at administrere siden, kontakt da en bidragyder eller 
+	send en besked på postlisten. Deltag i SliTaz samfundets 
+	<a href="http://scn.slitaz.org/groups/artwork/">grafikgruppe</a>
+</p>
+
+<!-- End of content -->
+</div>
+
+<!-- Footer -->
+<div id="footer">
+	Ophavsret &copy; <span class="year"></span>
+	<a href="http://www.slitaz.org/">SliTaz</a> - Netværk:
+	<a href="http://scn.slitaz.org/">Samfund</a>
+	<a href="http://doc.slitaz.org/">Vejledning</a>
+	<a href="http://forum.slitaz.org/">Forum</a>
+	<a href="http://pkgs.slitaz.org/">Programpakker</a>
+	<a href="http://bugs.slitaz.org">Fejl</a>
+	<a href="http://hg.slitaz.org/">Hg</a>
+	<p>
+		SliTaz @
+		<a href="http://twitter.com/slitaz">Twitter</a>
+		<a href="http://www.facebook.com/slitaz">Facebook</a>
+		<a href="http://distrowatch.com/slitaz">Distrowatch</a>
+		<a href="http://en.wikipedia.org/wiki/SliTaz">Wikipedia</a>
+		<a href="http://flattr.com/profile/slitaz">Flattr</a>
+	</p>
+</div>
+
+</body>
+</html>
