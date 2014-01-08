68a1cbcc43e5a850eb5f9ac9e9f72970930688eb
Christophe Lincoln <pankso@slitaz.org>
2014-01-08 11:11:43 +0100

    clean up main index, add files and now waiting on CGI/SHell support

---
+++ en/news/2011.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<title>SliTaz - News archives 2011</title>
+	<meta name="description" content="SliTaz news archives info release" />
+	<meta name="keywords" lang="en" content="System, free, gnu, linux, opensource software, livecd in RAM" />
+	<meta name="author" content="Christophe Lincoln"/>
+	<?php include("../../lib/html/meta-link.html"); ?>
+</head>
+<body>
+
+<?php include("../../lib/html/header.html"); ?>
+
+<!-- Block -->
+<div id="block">
+	<?php include("../../lib/html/nav.en.html"); ?>
+	<!-- Information/image -->
+	<div id="block_info">
+		<h4>News archives</h4>
+		<p>
+			Website news archives by year. Note that some links may be
+			broken due to website structure changes (mainly the docs
+			that moved to their own domain).
+		</p>
+		<p>
+			<img src="/images/news.png" alt="[ ]" />
+			<a href="2007.php">2007</a> | <a href="2008.php">2008</a>
+			| <a href="2009.php">2009</a> | <a href="2010.php">2010</a>
+			| <a href="2011.php">2011</a> | <a href="2012.php">2012</a>
+			| <a href="./">2014</a>
+		</p>
+	</div>
+</div>
+
+<?php include("../../lib/lang.php"); ?>
+
+<!-- Content -->
+<div id="content">
+
+<h2>2011</h2>
+
+<ul>
+	<li id="d20110531">
+		<strong>31 May 2011 - New and fresh Cooking 20110531</strong>
+	<p>
+		The Slitaz team is proud to announce the release of a new Cooking version
+		built with our new and fresh packages from cookutils. Cookutils is one of
+		the new generation of tools to build SliTaz packages and provides a Build
+		Bot with a nice web interface that works out-of-the-box on any SliTaz
+		system. These new packages have been built with a new i486 optimized
+		toolchain.
+	</p>
+	<p>
+		The full boot process has been improved and the first boot configuration
+		is now done through GTK boxes in a X session and the boot time is also
+		faster. This version comes with a new GUI tool for system configuration
+		as well as our new system configuration panel aka TazPanel. TazPanel
+		lets you configure the entire system via a xHTML/CSS web interface
+		(useful for remote control) and it also handles package management and
+		replaces the old GTK box.
+	</p>
+	<p>
+		Xorg now has dri support, so libdrm, linux-drm as well as linux-agp
+		are part of the core ISO. This adds 1MB to the compressed root
+		filesystem. But we managed to save a lot of space in the core ISO with
+		our new packages and this new Cooking is 30MB! This new cooking also
+		provides better internationalization and Italian support! Download:
+		<a href="http://mirror.slitaz.org/iso/cooking/slitaz-cooking.iso">
+			slitaz-cooking.iso</a>
+	</p>
+	</li>
+
+	<li id="d20110329">
+		<strong>29 March 2011 - New Cooking 20110329</strong>
+	<p>
+		The SliTaz team is pleased to announce the release of a new cooking
+		ISO featuring over 2900 packages. All packages have been rebuilt using
+		our new cooking tool, now included in tazwok. It contains Linux Kernel
+		2.6.37 compiled against glibc 2.13, binutils 2.21 &amp; gcc 4.5.2. The Core
+		LiveCD includes Midori 0.3.3. Tazpkg is now entirely translated to
+		French and we are open to additional translations. Tazwok has been
+		entirely rewritten and it's now possible to recook SliTaz from Scratch
+		using any ISO. This ISO is the first of a release candidate series which
+		leads us to a stable 4.0 release.
+	</p>
+	<p>
+		A lot of work has been done since our last cooking ISO released in november:
+		more than 2200 changes have been commited to the wok, and several
+		hundreds more to home-built tools. We are proud to announce that the SliTaz
+		community has grown a lot these last months: the last year, many
+		changes was commited than in the previous four. However, quantity doesn't mean
+		quality. We need you to help to test this new ISO and report eventual bugs. If
+		you want to, you are welcome to help fix known problems before 4.0 is released.
+		You can join members of the crew via our IRC, mailing-list, forums or by
+		mailing the packages maintainers. We hope you will enjoy this new
+		<a href="/en/get/">release!</a>
+	</p>
+	</li>
+</ul>
+
+<!-- End of content -->
+</div>
+
+<?php include("../../lib/html/footer.html"); ?>
+
+</body>
+</html>
---
+++ en/news/2012.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<title>SliTaz - News archives 2011</title>
+	<meta name="description" content="SliTaz news archives info release" />
+	<meta name="keywords" lang="en" content="System, free, gnu, linux, opensource software, livecd in RAM" />
+	<meta name="author" content="Christophe Lincoln"/>
+	<?php include("../../lib/html/meta-link.html"); ?>
+</head>
+<body>
+
+<?php include("../../lib/html/header.html"); ?>
+
+<!-- Block -->
+<div id="block">
+	<?php include("../../lib/html/nav.en.html"); ?>
+	<!-- Information/image -->
+	<div id="block_info">
+		<h4>News archives</h4>
+		<p>
+			Website news archives by year. Note that some links may be
+			broken due to website structure changes (mainly the docs
+			that moved to their own domain).
+		</p>
+		<p>
+			<img src="/images/news.png" alt="[ ]" />
+			<a href="2007.php">2007</a> | <a href="2008.php">2008</a>
+			| <a href="2009.php">2009</a> | <a href="2010.php">2010</a>
+			| <a href="2011.php">2011</a> | <a href="2012.php">2012</a>
+			| <a href="./">2014</a>
+		</p>
+	</div>
+</div>
+
+<?php include("../../lib/lang.php"); ?>
+
+<!-- Content -->
+<div id="content">
+
+<h2>2011</h2>
+
+<ul>
+	<li id="d20120410">
+		<strong>10 Apr 2012 - SliTaz GNU/Linux 4.0 Release</strong>
+	<p>
+		The SliTaz contributors are pleased to announce the release of 
+		the new stable distribution SliTaz GNU/Linux 4.0. Two years of 
+		community work have created a reliable system capable of even 
+		shorter boot times with more than 1000 new installable packages.
+	</p>
+	<p>
+		SliTaz provides a complete graphical desktop in 35 MB based
+		on LXDE and Openbox that works entirely in 192MB of RAM. Its 
+		new 4in1 CD image can be installed on a hard drive with only 48MB
+		by automatically selecting a configuration most fully suited to
+		your hardware.
+	</p>
+	<p>
+		The home-made tools have grown. Tazpkg brings a new notification
+		system and is much faster despite the increase in the number of 
+		packages and Tazpanel enables a new centralized management system. 
+		You can read the full <a href="/en/doc/releases/4.0/relnotes.en.html">Release Notes</a> 
+		and download SliTaz 4.0 from our mirror:
+		<a href="http://mirror.slitaz.org/iso/4.0/slitaz-4.0.iso">slitaz-4.0.iso</a>
+	</p>
+	</li>
+
+	<li id="d20120303">
+		<strong>03 Mar 2012 - SliTaz 4.0-RC2 is out</strong>
+	<p>
+		The Slitaz team is pleased to announce the new SliTaz 4.0-RC2 release
+		candidate. We have done a huge amount of work fixing bugs from RC1 and
+		on end-user home made tools such as TazPKG, TazPanel and TazUSB. Wifi
+		connection is made easier via TazPanel or the new wifibox. TazPKG has
+		better desktop integration and a new notification system.
+	</p>
+	<p>
+		This new version comes with a full Xorg package and DRI support as
+		well as the nv, intel, geode and vesa drivers working out-of-the-box.
+		We have also rebuilt all packages once again and saved ~2Mb in the
+		core LiveCD, so we now include Lxrandr, Parcellite and the
+		transmission BiTorrent client.
+	</p>
+	<p>
+		We want to thank everyone who tested the first release candidate; this
+		RC2 is close to the upcoming SliTaz 4.0 version planned in 2 weeks.
+		Download the RC2 ISO image from our mirror (35,1 Mb):
+		<a href="http://mirror.slitaz.org/iso/cooking/slitaz-4.0-RC2.iso">SliTaz 4.0-RC2</a>
+	</p>
+	</li>
+
+	<li id="d20120223">
+		<strong>23 Feb 2012 - SliTaz 4.0 -RC1 is out</strong>
+	<p>
+		The SliTaz contributor team is pleased to announce the immediate
+		availability of the first release candidate for SliTaz 4.0. SliTaz can
+		boot in 10-12 seconds on fairly new hardware and with the new 4in1 ISO
+		you can install a full SliTaz desktop with only 48MB of RAM. The 4in1
+		ISO is able to auto detect the memory and can boot SliTaz in text mode,
+		to a minimal X environment or a full graphical desktop.
+	</p>
+	<p>
+		SliTaz 4.0 will have more than 3000 well tested packages in its
+		repository and the RC series lets us track the latest bugs. From our last
+		Cooking release the packages have all been rebuilt twice to ensure build
+		quality.
+	</p>
+	<p>
+		This RC2 ISO will prompt you with a new graphical menu and a graphical
+		language and keyboard set up. The ISO features a new desktop layout and
+		artwork as well as a new command line installer with a CGI/web interface
+		accessible via TazPanel. You can download this new ISO from our mirror:
+		<a href="http://mirror.slitaz.org/iso/cooking/slitaz-4.0-RC1.iso">SliTaz 4.0-RC1</a>
+	</p>
+	</li>
+</ul>
+
+<!-- End of content -->
+</div>
+
+<?php include("../../lib/html/footer.html"); ?>
+
+</body>
+</html>
