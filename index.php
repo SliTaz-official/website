ec407fbefea28277786b4352804749a86d07cd39
Christophe Lincoln <pankso@slitaz.org>
2017-03-24 23:15:18 +0100

    Some clean-up (rm old symlimks) and content negociation in /index.php

--- index.php
+++ index.php
@@ -1,6 +1,45 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta http-equiv="refresh" content="0; URL=en/">
-</head>
-</html>
+<?php
+
+$lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
+
+switch ($lang) {
+	case "bs":
+		echo "<meta http-equiv='refresh' content='0; URL=bs/'>";
+		break;
+	case "cn":
+		echo "<meta http-equiv='refresh' content='0; URL=cn/'>";
+		break;
+	case "da":
+		echo "<meta http-equiv='refresh' content='0; URL=da/'>";
+		break;
+	case "de":
+		echo "<meta http-equiv='refresh' content='0; URL=de/'>";
+		break;
+	case "en":
+		echo "<meta http-equiv='refresh' content='0; URL=en/'>";
+		break;
+	case "es":
+		echo "<meta http-equiv='refresh' content='0; URL=es/'>";
+		break;
+	case "fr":
+		echo "<meta http-equiv='refresh' content='0; URL=fr/'>";
+		break;
+	case "id":
+		echo "<meta http-equiv='refresh' content='0; URL=id/'>";
+		break;
+	case "it":
+		echo "<meta http-equiv='refresh' content='0; URL=it/'>";
+		break;
+	case "ja":
+		echo "<meta http-equiv='refresh' content='0; URL=ja/'>";
+		break;
+	case "pt":
+		echo "<meta http-equiv='refresh' content='0; URL=pt/'>";
+		break;
+	case "ru":
+		echo "<meta http-equiv='refresh' content='0; URL=ru/'>";
+		break;
+	default:
+		echo "<meta http-equiv='refresh' content='0; URL=en/'>";
+		break;
+}
