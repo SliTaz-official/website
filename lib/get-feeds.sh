119e87a4355c851fc9345dc0caae0770af8e7d68
Christophe Lincoln <pankso@slitaz.org>
2017-03-25 00:32:07 +0100

    Add Blog to top menu - fix rss feeds and dl in website/cache

--- lib/get-feeds.sh
+++ lib/get-feeds.sh
@@ -1,25 +1,23 @@
 #!/bin/sh
 #
-# Get latest commits and feeds to display on the website main page.
+# Get latest commits and feeds to display on the website main pages.
 # On the server, this script is executed by cron each hour.
 #
-
-CACHE="/var/cache/slitaz/website"
+PWD=$(dirname $0)
+CACHE="$(dirname $PWD)/cache"
 
 # Feeds URL http://scn.slitaz.org/activity/feed/
-SCN_FEED='http://scn.slitaz.org/activity/feed/'
-BLOG_FEED='http://scn.slitaz.org/category/news/feed/'
+BLOG_FEED='http://scn.slitaz.org/?blog=rss'
 WOK_FEED='http://hg.slitaz.org/wok/rss-log'
 FORUM_FEED='http://forum.slitaz.org/rss'
 ROLLING_DATE='http://mirror1.slitaz.org/rolling-date.sh'
 
 # Clean cache
-mkdir -p $CACHE && cd $CACHE
+mkdir -p ${CACHE} && cd ${CACHE}
 rm -f *.xml
 
 # Cache all feeds to save bandwidth (updated by cron)
 echo -n "Getting latest rss feeds... "
-wget -O scn.xml $SCN_FEED 2>/dev/null
 wget -O wok.xml $WOK_FEED 2>/dev/null
 wget -O blog.xml $BLOG_FEED 2>/dev/null
 wget -O forum.xml $FORUM_FEED 2>/dev/null
